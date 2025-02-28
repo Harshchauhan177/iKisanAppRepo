85525095766ec18b6bee0ec5186c93e039f3a3a0
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-02-28 14:03:55 +0530

    In profile Help and Payment Complete

---
+++ iKisanApp/iKisanApp/FolderForAgriAssist/profile/helpViewController.swift
@@ -0,0 +1,28 @@
+//
+//  helpViewController.swift
+//  iKisanApp
+//
+//  Created by Batch - 1 on 28/02/25.
+//
+
+import UIKit
+
+class helpViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "mail us"
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func emailTapped(_ sender: UIButton) {
+        if let url = URL(string: "mailto:support@example.com"){
+            UIApplication.shared.open(url)
+        }
+    }
+    
+    
+   
+}
+//UIApplication.shared.open(url)
