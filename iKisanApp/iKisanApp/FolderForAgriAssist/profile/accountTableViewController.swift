2850bb6c76c5eb0b99bb7d09c8f3de1262543317
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-03-04 01:22:48 +0530

    in account working on setting

--- iKisanApp/iKisanApp/FolderForAgriAssist/profile/accountTableViewController.swift
+++ iKisanApp/iKisanApp/FolderForAgriAssist/profile/accountTableViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class accountTableViewController: UITableViewController {
-
+    @IBOutlet weak var singOutcell: UITableViewCell!
+    
     @IBOutlet weak var imageView: UIImageView!
     
     @IBOutlet weak var nameLabel: UILabel!
@@ -51,7 +52,19 @@ class accountTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-   
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let singOutCellIndexPath = tableView.indexPath(for: singOutcell), indexPath == singOutCellIndexPath {
+            showSignOutAlert()
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    private func showSignOutAlert() {
+        let alert = UIAlertController(title: "Sign Out Account", message: "Are you sure you want to sign Out your account?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler:nil))
+        present(alert, animated: true, completion: nil)
+    }
 
 
 }
