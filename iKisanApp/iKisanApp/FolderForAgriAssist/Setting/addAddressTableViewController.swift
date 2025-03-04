f9441138d989e7761688097d63a685a076cdc67c
Harshchauhan177 <harsh7617rajput@gmail.com>
2025-03-04 13:21:20 +0530

    in setting working on Add address

---
+++ iKisanApp/iKisanApp/FolderForAgriAssist/Setting/addAddressTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  addAddressTableViewController.swift
+//  iKisanApp
+//
+//  Created by Batch - 1 on 04/03/25.
+//
+
+import UIKit
+
+class addAddressTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
---
+++ iKisanApp/iKisanApp/FolderForAgriAssist/Setting/notificationsTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  notificationsTableViewController.swift
+//  iKisanApp
+//
+//  Created by Batch - 1 on 04/03/25.
+//
+
+import UIKit
+
+class notificationsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+}
